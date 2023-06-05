e4e254753d80d3bc8cdd771a5cae5fee316d4262
SL <82523453+Swarnlataaa@users.noreply.github.com>
2023-06-05 16:26:58 +0530

    Update Decision_tree_reg.py

--- Decision_tree_reg.py
+++ Decision_tree_reg.py
@@ -1,22 +1,29 @@
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.datasets import make_regression
+from sklearn.tree import export_graphviz
+import pydot
 
-# Load your dataset (X: features, y: target values)
-X, y = load_dataset()
+# Generate a synthetic regression dataset for demonstration
+X, y = make_regression(n_samples=100, n_features=1, noise=0.2, random_state=42)
 
-# Split the dataset into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Create a Decision Tree Regressor object
+# Create a Decision Tree Regressor
 tree_reg = DecisionTreeRegressor(max_depth=3)
+tree_reg.fit(X, y)
 
-# Train the model
-tree_reg.fit(X_train, y_train)
+# Export the decision tree to a Graphviz format
+dot_data = export_graphviz(tree_reg, out_file=None, filled=True, rounded=True, special_characters=True)
 
-# Make predictions on the test set
-y_pred = tree_reg.predict(X_test)
+# Create a Graphviz graph from the DOT data
+graph = pydot.graph_from_dot_data(dot_data)[0]
 
-# Calculate mean squared error
-mse = mean_squared_error(y_test, y_pred)
-print("Mean Squared Error:", mse)
+# Create a Matplotlib figure and axis
+fig, ax = plt.subplots(figsize=(6, 6))
+
+# Convert the graph to a Matplotlib image and display it
+ax.imshow(graph.create_png(), aspect='equal')
+
+# Remove the axis labels
+ax.axis('off')
+
+# Show the plot
+plt.show()
